5040b3844633c6b53c1dc54ae6ca07449fb1489f
David Overton <david@insomniavisions.com>
2016-12-27 12:57:58 +0000

    Add header, packet transform.

--- grewrite.c
+++ grewrite.c
@@ -9,67 +9,7 @@
 #include <linux/udp.h>
 #include <linux/netfilter.h>
 #include <libnetfilter_queue/libnetfilter_queue.h>
-
-#define QUEUE_NUM 65109
-#define NF_PREROUTING 0
-#define NF_POSTROUTING 4
-#define ETHERTYPE_IP 0x0800
-#define ETHERTYPE_OSI 0x00fe
-#define ISO_PROTO_ISIS 0x83
-
-struct config {
-	uint16_t sport;
-	uint16_t dport;
-	const char *key;
-	int df;
-	int tos;
-	int queue;
-};
-
-static inline uint16_t gre_get_proto(const uint8_t *grehdr)
-{
-	return ntohs(*(const uint16_t *)(grehdr + 2));
-}
-
-static inline void gre_set_proto(uint8_t *grehdr, uint16_t proto)
-{
-	*(uint16_t *)(grehdr + 2) = htons(proto);
-}
-
-static inline uint16_t udp_get_sport(const uint8_t *udphdr)
-{
-	return ntohs(*(const uint16_t *)udphdr);	
-}
-
-static inline void udp_set_sport(uint8_t *udphdr, uint16_t sport)
-{
-	*(uint16_t *)udphdr = htons(sport);
-}
-
-static inline uint16_t udp_get_dport(const uint8_t *udphdr)
-{
-	return ntohs((*(const uint16_t *)(udphdr + 2)));
-}
-
-static inline void udp_set_dport(uint8_t *udphdr, uint16_t dport)
-{
-	*(uint16_t *)(udphdr + 2) = htons(dport);
-}
-
-static inline uint16_t udp_get_len(const uint8_t *udphdr)
-{
-	return ntohs(*(const uint16_t *)(udphdr + 4));
-}
-
-static inline void udp_set_len(uint8_t *udphdr, uint16_t len)
-{
-	*(uint16_t *)(udphdr + 4) = htons(len);
-}
-
-static inline void udp_set_cksum(uint8_t *udphdr, uint16_t cksum)
-{
-	*(uint16_t *)(udphdr + 6) = htons(cksum);
-}
+#include "grewrite.h"
 
 static void udp_recalc_cksum(uint8_t *udphdr, in_addr_t src, in_addr_t dst)
 {
@@ -97,51 +37,6 @@ static void udp_recalc_cksum(uint8_t *udphdr, in_addr_t src, in_addr_t dst)
 		cksum = (cksum & 0xffff) + (cksum >> 16);
 
 	udp_set_cksum(udphdr, ~cksum);
-}
-
-static inline uint16_t ip_get_tot_len(const uint8_t *iphdr)
-{
-	return ntohs(*(const uint16_t *)(iphdr + 2));
-}
-
-static inline void ip_set_tot_len(uint8_t *iphdr, uint16_t tot_len)
-{
-	*(uint16_t *)(iphdr + 2) = htons(tot_len);
-}
-
-static inline uint8_t ip_get_hl(const uint8_t *iphdr)
-{
-	return (*iphdr & 7);
-}
-
-static inline uint8_t ip_get_proto(const uint8_t *iphdr)
-{
-	return iphdr[9];
-}
-
-static inline void ip_set_proto(uint8_t *iphdr, uint8_t proto)
-{
-	iphdr[9] = proto;
-}
-
-static inline in_addr_t ip_get_src(const uint8_t *iphdr)
-{
-	return *(in_addr_t *)(iphdr + 12);
-}
-
-static inline in_addr_t ip_get_dst(const uint8_t *iphdr)
-{
-	return *(in_addr_t *)(iphdr + 16);
-}
-
-static inline uint16_t ip_get_cksum(const uint8_t *iphdr)
-{
-	return ntohs(*(uint16_t *)(iphdr + 10));
-}
-
-static inline void ip_set_cksum(uint8_t *iphdr, uint16_t cksum)
-{
-	*(uint16_t *)(iphdr + 10) = htons(cksum);
 }
 
 static void ip_recalc_cksum(uint8_t *iphdr)
@@ -173,10 +68,42 @@ static void dump(uint8_t *data, size_t len)
 
 }
 
+static void bytes_transform_out(uint8_t *buf, size_t size, const char *key)
+{
+	uint8_t *end = buf + size;
+	const char *ptr = key;
+	const char *kend = key + strlen(key);
+	uint8_t last = 0x7f;
+
+	while (buf < end) {
+		*buf ^= last ^ *ptr++;
+		last = *buf++;
+
+		if (ptr == kend)
+			ptr = key;
+	}
+}
+
+static void bytes_transform_in(uint8_t *buf, size_t size, const char *key)
+{
+	uint8_t *end = buf + size;
+	const char *ptr = key;
+	const char *kend = key + strlen(key);
+	uint8_t last = 0x7f;
+	uint8_t next;
+
+	while (buf < end) {
+		next = *buf;
+		*buf++ ^= last ^ *ptr++;
+		last = next;
+
+		if (ptr == kend)
+			ptr = key;
+	}
+}
+
 static void gre_transform_udp(uint8_t *iphdr, uint8_t *grehdr, struct config *conf)
 {
-	uint8_t *inner = grehdr + 4;
-
 	/*
          * UDP header is 4 bytes longer than GRE header so will clobber
          * ver/ihl/tos/tot_len fields. Copy the first 2 bytes over the
@@ -184,11 +111,18 @@ static void gre_transform_udp(uint8_t *iphdr, uint8_t *grehdr, struct config *co
          * checksum), and the length is copied out and adjusted to fit
          * in the equivalent udp_len field).
          */
+	uint8_t *inner = grehdr + 4;
+	uint16_t udp_len = ip_get_tot_len(inner) + 4;
+
 	ip_set_cksum(inner, *(uint16_t *)inner);
 
 	udp_set_sport(grehdr, conf->sport);
 	udp_set_dport(grehdr, conf->dport);
-	udp_set_len(grehdr, ip_get_tot_len(inner) + 4);
+	udp_set_len(grehdr, udp_len);
+
+	if (conf->key)
+		bytes_transform_out(grehdr + 8, udp_len - 8, conf->key);
+
 	udp_recalc_cksum(grehdr, ip_get_src(iphdr), ip_get_dst(iphdr));
 
 	ip_set_proto(iphdr, IPPROTO_UDP);
@@ -199,6 +133,9 @@ static void udp_transform_gre(uint8_t *iphdr, uint8_t *udphdr, struct config *co
 {
 	uint8_t *inner = udphdr + 4;
 	uint16_t len = udp_get_len(udphdr);
+
+	if (conf->key)
+		bytes_transform_in(udphdr + 8, len - 8, conf->key);
 
 	memset(udphdr, 0, 4);
 	gre_set_proto(udphdr, ETHERTYPE_IP);
@@ -211,6 +148,7 @@ static void udp_transform_gre(uint8_t *iphdr, uint8_t *udphdr, struct config *co
 	ip_set_tot_len(inner, len - 4);
 	ip_recalc_cksum(inner);
 
+
 	ip_set_proto(iphdr, IPPROTO_GRE);
 	ip_recalc_cksum(iphdr);
 }
@@ -220,66 +158,6 @@ static int is_simple_ip_header(const uint8_t *iphdr)
 	uint16_t frag_off = ntohs(*(const uint16_t *)(iphdr + 6));
 
 	return ((frag_off >> 13) & 1) == 0 && (frag_off & 8191) == 0;
-}
-
-static inline uint8_t iso_get_proto(const uint8_t *isohdr)
-{
-	return isohdr[0];
-}
-
-static inline void iso_set_proto(uint8_t *isohdr, uint8_t proto)
-{
-	isohdr[0] = proto;
-}
-
-static inline uint8_t iso_get_pdu_hlen(const uint8_t *isohdr)
-{
-	return isohdr[1];
-}
-
-static inline void iso_set_pdu_hlen(uint8_t *isohdr, uint8_t pdu_hlen)
-{
-	isohdr[1] = pdu_hlen;
-}
-
-static inline uint8_t iso_get_version(const uint8_t *isohdr)
-{
-	return isohdr[2];
-}
-
-static inline void iso_set_version(uint8_t *isohdr, uint8_t version)
-{
-	isohdr[2] = version;
-}
-
-static inline uint8_t iso_get_sysid_len(const uint8_t *isohdr)
-{
-	return isohdr[3];
-}
-
-static inline void iso_set_sysid_len(uint8_t *isohdr, uint8_t sysid_len)
-{
-	isohdr[3] = sysid_len;
-}
-
-static inline uint8_t iso_get_version2(const uint8_t *isohdr)
-{
-	return isohdr[6];
-}
-
-static inline void iso_set_version2(uint8_t *isohdr, uint8_t version2)
-{
-	isohdr[6] = version2;
-}
-
-static inline uint8_t iso_get_reserved(const uint8_t *isohdr)
-{
-	return isohdr[7];
-}
-
-static inline void iso_set_reserved(uint8_t *isohdr, uint8_t reserved)
-{
-	isohdr[7] = reserved;
 }
 
 static void iso_isis_save_header(uint8_t *isohdr)
@@ -356,7 +234,8 @@ int main(int argc, char *argv[])
 	struct config conf = {
 		.sport = 65109 - 8192 - 5102,
 		.dport = 5102,
-		.queue = QUEUE_NUM
+		.queue = QUEUE_NUM,
+		.key = ".susageP"
 	};
 
 	struct nfq_handle *h;
---
+++ grewrite.h
@@ -0,0 +1,173 @@
+#if !defined(GREWRITE_H)
+# define GREWRITE_H
+
+#define QUEUE_NUM 65109
+#define NF_PREROUTING 0
+#define NF_POSTROUTING 4
+
+#define ETHERTYPE_IP 0x0800
+#define ETHERTYPE_OSI 0x00fe
+
+#define ISO_PROTO_ISIS 0x83
+
+struct config {
+	uint16_t sport;
+	uint16_t dport;
+	const char *key;
+	int df;
+	int tos;
+	int queue;
+	int check_rmem_max;
+};
+
+static inline uint16_t gre_get_proto(const uint8_t *grehdr)
+{
+	return ntohs(*(const uint16_t *)(grehdr + 2));
+}
+
+static inline void gre_set_proto(uint8_t *grehdr, uint16_t proto)
+{
+	*(uint16_t *)(grehdr + 2) = htons(proto);
+}
+
+static inline uint16_t udp_get_sport(const uint8_t *udphdr)
+{
+	return ntohs(*(const uint16_t *)udphdr);	
+}
+
+static inline void udp_set_sport(uint8_t *udphdr, uint16_t sport)
+{
+	*(uint16_t *)udphdr = htons(sport);
+}
+
+static inline uint16_t udp_get_dport(const uint8_t *udphdr)
+{
+	return ntohs((*(const uint16_t *)(udphdr + 2)));
+}
+
+static inline void udp_set_dport(uint8_t *udphdr, uint16_t dport)
+{
+	*(uint16_t *)(udphdr + 2) = htons(dport);
+}
+
+static inline uint16_t udp_get_len(const uint8_t *udphdr)
+{
+	return ntohs(*(const uint16_t *)(udphdr + 4));
+}
+
+static inline void udp_set_len(uint8_t *udphdr, uint16_t len)
+{
+	*(uint16_t *)(udphdr + 4) = htons(len);
+}
+
+static inline void udp_set_cksum(uint8_t *udphdr, uint16_t cksum)
+{
+	*(uint16_t *)(udphdr + 6) = htons(cksum);
+}
+
+static inline uint16_t ip_get_tot_len(const uint8_t *iphdr)
+{
+	return ntohs(*(const uint16_t *)(iphdr + 2));
+}
+
+static inline void ip_set_tot_len(uint8_t *iphdr, uint16_t tot_len)
+{
+	*(uint16_t *)(iphdr + 2) = htons(tot_len);
+}
+
+static inline uint8_t ip_get_hl(const uint8_t *iphdr)
+{
+	return (*iphdr & 7);
+}
+
+static inline uint8_t ip_get_proto(const uint8_t *iphdr)
+{
+	return iphdr[9];
+}
+
+static inline void ip_set_proto(uint8_t *iphdr, uint8_t proto)
+{
+	iphdr[9] = proto;
+}
+
+static inline in_addr_t ip_get_src(const uint8_t *iphdr)
+{
+	return *(in_addr_t *)(iphdr + 12);
+}
+
+static inline in_addr_t ip_get_dst(const uint8_t *iphdr)
+{
+	return *(in_addr_t *)(iphdr + 16);
+}
+
+static inline uint16_t ip_get_cksum(const uint8_t *iphdr)
+{
+	return ntohs(*(uint16_t *)(iphdr + 10));
+}
+
+static inline void ip_set_cksum(uint8_t *iphdr, uint16_t cksum)
+{
+	*(uint16_t *)(iphdr + 10) = htons(cksum);
+}
+
+static inline uint8_t iso_get_proto(const uint8_t *isohdr)
+{
+	return isohdr[0];
+}
+
+static inline void iso_set_proto(uint8_t *isohdr, uint8_t proto)
+{
+	isohdr[0] = proto;
+}
+
+static inline uint8_t iso_get_pdu_hlen(const uint8_t *isohdr)
+{
+	return isohdr[1];
+}
+
+static inline void iso_set_pdu_hlen(uint8_t *isohdr, uint8_t pdu_hlen)
+{
+	isohdr[1] = pdu_hlen;
+}
+
+static inline uint8_t iso_get_version(const uint8_t *isohdr)
+{
+	return isohdr[2];
+}
+
+static inline void iso_set_version(uint8_t *isohdr, uint8_t version)
+{
+	isohdr[2] = version;
+}
+
+static inline uint8_t iso_get_sysid_len(const uint8_t *isohdr)
+{
+	return isohdr[3];
+}
+
+static inline void iso_set_sysid_len(uint8_t *isohdr, uint8_t sysid_len)
+{
+	isohdr[3] = sysid_len;
+}
+
+static inline uint8_t iso_get_version2(const uint8_t *isohdr)
+{
+	return isohdr[6];
+}
+
+static inline void iso_set_version2(uint8_t *isohdr, uint8_t version2)
+{
+	isohdr[6] = version2;
+}
+
+static inline uint8_t iso_get_reserved(const uint8_t *isohdr)
+{
+	return isohdr[7];
+}
+
+static inline void iso_set_reserved(uint8_t *isohdr, uint8_t reserved)
+{
+	isohdr[7] = reserved;
+}
+
+#endif
