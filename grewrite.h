cc57aefe23690b84f3fd2428a4c966d85a950872
David Overton <david@insomniavisions.com>
2017-01-29 21:01:13 +0000

    Add licence.

--- grewrite.h
+++ grewrite.h
@@ -1,3 +1,21 @@
+/*
+ * (C) 2017 David Overton <david@insomniavisions.com>
+ *
+ * This file is part of grewrite.
+ *
+ * grewrite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * grewrite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with grewrite.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #if !defined(GREWRITE_H)
 # define GREWRITE_H
 
@@ -277,3 +295,4 @@ static inline void iso_esis_set_cksum(uint8_t *isohdr, uint16_t cksum)
 int transform_ip_packet(uint8_t *iphdr, size_t size, struct config *conf);
 
 #endif
+
